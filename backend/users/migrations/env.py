a038d3f15ac5f06171c47a1c95079fc4186f71ee
Max Bazarov <bazaroffm@gmail.com>
2023-04-02 18:15:21 +0300

    add common metadata

--- backend/users/migrations/env.py
+++ backend/users/migrations/env.py
@@ -2,16 +2,14 @@ import os
 import sys
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
 sys.path.append(os.path.join(sys.path[0], 'src'))
 
+from src.auth.models import *
 from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-from src.auth.models import metadata as metadata_auth
-from src.operations.models import metadata as metadata_operations
+from src.database import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -32,7 +30,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [metadata_auth, metadata_operations]
+target_metadata = metadata
 # target_metadata = [metadata, Base.metadata]
 
 # other values from the config, defined by the needs of env.py,
