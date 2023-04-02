e59d651f9a9e47337daa2f8c9596921db97da4c8
Max Bazarov <bazaroffm@gmail.com>
2023-04-02 18:11:57 +0300

    isort and metadata

--- backend/users/src/database.py
+++ backend/users/src/database.py
@@ -1,16 +1,11 @@
 # Единая точка входа в БД. Эта точка будет переиспользоваться в других файлах
 from typing import AsyncGenerator
-# from sqlalchemy import MetaData
-
-from sqlalchemy.ext.asyncio import (
-    AsyncSession,
-    async_sessionmaker,
-    create_async_engine,
-)
-from sqlalchemy.orm import DeclarativeBase
 
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
+                                    create_async_engine)
+from sqlalchemy.orm import DeclarativeBase
 
 DATABASE_URL = (
     f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
@@ -20,6 +15,8 @@ DATABASE_URL = (
 class Base(DeclarativeBase):
     pass
 
+
+metadata = MetaData()
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
