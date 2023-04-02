2cefcea5557f0831877ea9ad5316f8ee2f636186
Max Bazarov <bazaroffm@gmail.com>
2023-04-02 18:24:39 +0300

    add car schemas

--- backend/rides/src/rides/schemas/rides.py
+++ backend/rides/src/rides/schemas/rides.py
@@ -1,6 +1,22 @@
+from typing import Optional
 from pydantic import BaseModel
+
+
+class CarRead(BaseModel):
+    id: int
+    make: str
+    model: str
+    year: int
+    license_plate_number: str
+
+
+class CarCreate(BaseModel):
+    make: str
+    model: str
+    year: Optional[int] = None
+    license_plate_number: str
 
 
 class RideRead(BaseModel):
     driver: int
-    car: 
+    car: CarRead
